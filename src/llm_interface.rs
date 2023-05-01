60fcee557af027b899570ff59cdc8d588a4c40d0
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2023-05-01 20:21:32 +0200

    Added LLM options, added mutex, cleanup

--- src/llm_interface.rs
+++ src/llm_interface.rs
@@ -2,15 +2,20 @@ use crate::error::LLMError;
 use llm_chain::{parameters, prompt, traits::Executor};
 use llm_chain_llama::Executor as LlamaExecutor;
 use llm_chain_openai::chatgpt::Executor as ChatGPTExecutor;
-use std::error::Error;
+
+use llm_chain_llama::{PerExecutor, PerInvocation};
 
 pub struct LLMInterface<T: Executor> {
     exec: T,
     is_busy: bool,
 }
 impl LLMInterface<LlamaExecutor> {
-    pub fn with_llama_executor() -> Result<Self, LLMError> {
-        let executor = LlamaExecutor::new().map_err(|_| LLMError::InitializingLLMFailed)?;
+    pub fn new_local_llm(model_path: &str) -> Result<Self, LLMError> {
+        let exec_options = PerExecutor::new().with_model_path(model_path);
+        let mut inv_options = PerInvocation::new();
+        inv_options.n_threads = Some(8);
+        let executor = LlamaExecutor::new_with_options(Some(exec_options), Some(inv_options))
+            .map_err(|_| LLMError::InitializingLLMFailed)?;
 
         Ok(Self {
             exec: executor,
--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,35 @@ use llm_interface::LLMInterface;
 use std::error::Error;
 use std::sync::Arc;
 
-async fn handle_request(
+use tokio::sync::Mutex;
+
+async fn prompt_request(
     req: Request<Body>,
-    llm: Arc<LLMInterface<LlamaExecutor>>,
+    llm: Arc<Mutex<LLMInterface<LlamaExecutor>>>,
 ) -> Result<Response<Body>, LLMError> {
-    let mut llm = LLMInterface::with_llama_executor()?;
-    let content = llm.submit_prompt().await?;
+    let mut llm_guard = llm.lock().await;
+    let content = llm_guard.submit_prompt().await?;
 
     Ok(Response::new(Body::from(content)))
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Setup LLMInterface using an Arc to enable sharing the LLM across endpoints
-    let llm = Arc::new(LLMInterface::with_llama_executor()?);
+    // Setup LLMInterface using an Arc and Mutex to enable sharing the LLM interface across endpoints
+    let llm = Arc::new(Mutex::new(LLMInterface::new_local_llm("model.bin")?));
 
-    // Setup the actual webserver
-    let make_svc = make_service_fn(|_conn| {
+    // Setup the endpoints
+    let prompt_svc = make_service_fn(|_conn| {
         let llm = Arc::clone(&llm);
         async move {
-            Ok::<_, hyper::Error>(service_fn(move |req| handle_request(req, Arc::clone(&llm))))
+            Ok::<_, hyper::Error>(service_fn(move |req| prompt_request(req, Arc::clone(&llm))))
         }
     });
 
+    // Start the server
     let addr = ([127, 0, 0, 1], 8080).into();
-    let server = Server::bind(&addr).serve(make_svc);
-
+    let server = Server::bind(&addr).serve(prompt_svc);
     println!("Server is running on http://{}", addr);
-
     if let Err(e) = server.await {
         eprintln!("Server error: {}", e);
     }
